180c83dad07da9b852e56c1a0b6b92ad46e444be
Guilherme Guerra <gguerragui@gmail.com>
2012-05-28 02:00:15 -0300

    Compass, sprites

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '26e568b71d48f72dfbba6614ff39727904c47c794fb1378fd239eb02ba1540d3259d0486760c2207fa370ea70e887773a045005c35d5c2447e69d5269ee9e5ec'
---
+++ config/routes.rb~
@@ -0,0 +1,3 @@
+Demo::Application.routes.draw do
+	root :to => 'home#index'
+end
